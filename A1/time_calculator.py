60293f8da1c9d9eec7241e6c5dbf7bd282ada7d9
marlonfajardo <marlonrfajardo@gmail.com>
2019-09-29 13:45:30 -0700

    Program finished, now to clean up and write docstrings

--- A1/time_calculator.py
+++ A1/time_calculator.py
@@ -1,3 +1,6 @@
+import doctest
+
+
 def time_calculator(input_seconds):
     """
 
@@ -5,71 +8,86 @@ def time_calculator(input_seconds):
     :return:
     """
     days = find_days(input_seconds)
-    remainder = find_remainder(input_seconds, days)
-    hours = find_hours(remainder)
-    remainder = find_remainder(remainder, hours)
-    minutes = find_minutes(remainder)
-    remiander = find_remainder(remainder, minutes)
-    seconds = remainder
-    print(format_time(days, hours, minutes, seconds))
+    remainder_days = find_remainder(input_seconds, days, 86400)
+    hours = find_hours(remainder_days)
+    remainder_hours = find_remainder(remainder_days, hours, 3600)
+    minutes = find_minutes(remainder_hours)
+    remainder_minutes = find_remainder(remainder_hours, minutes, 60)
+    seconds = remainder_minutes
+
+    format_time(days, hours, minutes, seconds)
 
 
+def find_remainder(original_value, converted_value, multiplier):
+    """
 
-def find_days(seconds):
+    :param original_value:
+    :param subtracting_value:
+    :return:
+    """
+    excess_value = original_value - (converted_value * multiplier)
+    return excess_value
+
+def find_days(days_seconds):
     """
 
     :return:
     """
-    if seconds > 86400:
-        days = seconds / 86400 - seconds % 86400
+    if days_seconds > 86400:
+        days = int(days_seconds / 86400)
         return days
-    elif seconds < 86400:
-        return seconds
+    elif days_seconds < 86400:
+        return 0
 
 
-def find_hours(seconds):
+def find_hours(hours_seconds):
     """
 
     :return:
     """
-    if seconds > 3600:
-        hours = seconds / 3600 - seconds % 3600
-        return days
-    elif seconds < 3600:
-        return seconds
+    if hours_seconds > 3600:
+        hours = int(hours_seconds / 3600)
+        return hours
+    elif hours_seconds < 3600:
+        return 0
 
 
-def find_minutes(seconds):
+def find_minutes(minute_seconds):
     """
 
     :return:
     """
-    if seconds > 60:
-        hours = seconds / 60 - seconds % 60
-        return days
-    elif seconds < 60:
-        return seconds
+    if minute_seconds > 60:
+        minutes = int(minute_seconds / 60)
+        return minutes
+    elif minute_seconds < 60:
+        return 0
 
 
-def format_time(days, hours, minutes, seconds):
+def format_time(days_int, hours_int, minutes_int, seconds_int):
     """
 
     :return:
     """
-    formatted_time = (days + "" + hours + "" + minutes + "" + seconds)
+    formatted_time = (str(days_int) + "days, " + str(hours_int) + "hours, " + str(minutes_int) + "minutes, " + str(seconds_int) + "seconds")
 
-    return formatted_time
+    print(formatted_time)
 
 def main():
     """
 
     :return:
     """
-    time_calculator(172810)
+
+    print(time_calculator(1382380))
+    print(time_calculator(1))
+    print(time_calculator(5678))
 
 
 
-if __name__ == 'main':
+
+
+if __name__ == '__main__':
     main()
 
 
