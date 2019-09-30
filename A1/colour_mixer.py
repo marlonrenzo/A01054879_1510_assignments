8091fc57587c8d13c5e832c55760657fb8412856
marlonfajardo <marlonrfajardo@gmail.com>
2019-09-29 19:08:44 -0700

    Deconstructed into 4 functions.

--- A1/colour_mixer.py
+++ A1/colour_mixer.py
@@ -3,23 +3,56 @@ import doctest
 def colour_mixer():
     input_one = input("Enter a primary colour here:\n")
     input_two = input("Enter a second primary colour here:\n")
-    input_one = input_one.lower()
-    input_one = input_one.strip()
-    input_two = input_two.lower()
-    input_two = input_two.strip()
 
-    string = input_one + input_two
+    colour = add_colours(input_one, input_two)
+    return colour
 
-    string = string.replace("bluered", "Purple")
-    string = string.replace("redblue", "Purple")
-    string = string.replace("yellowblue", "Green")
-    string = string.replace("blueyellow", "Green")
-    string = string.replace("redyellow", "Orange")
-    string = string.replace("redyellow", "Orange")
+
+def add_colours(first_string, second_string):
+    first_string = format_input(first_string)
+    second_string = format_input(second_string)
+
+    string = first_string + second_string
+
+    colour = colour_replace(string)
+    valid_colour = check_colour(colour)
+    if valid_colour == True:
+        return colour
+    else:
+        return valid_colour
+
+
+def check_colour(string):
+    if string == "Purple" or string ==  "Green" or string ==  "Orange":
+        return True
+    else:
+        return "Your entry is not valid, ensure you're adding different primary colours."
+
+
+def format_input(string):
+    string = string.lower()
+    string = string.strip()
 
     return string
 
 
+def colour_replace(combination):
+    """
+    Replace the added colours into a secondary colour.
+
+    :precondition: combination must be a string matching one of the following listen below
+    :postcondition: will return a secodnary colour as a string
+    :param combination: the added colours as a string
+    :return: a string
+    """
+    combination = combination.replace("bluered", "Purple")
+    combination = combination.replace("redblue", "Purple")
+    combination = combination.replace("yellowblue", "Green")
+    combination = combination.replace("blueyellow", "Green")
+    combination = combination.replace("redyellow", "Orange")
+    combination = combination.replace("yellowred", "Orange")
+
+    return combination
 
 
 
