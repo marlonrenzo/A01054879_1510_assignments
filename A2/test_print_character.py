7ef717fed5cd27749a5df498b72598264f14d7ed
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-17 11:00:14 -0700

    modified length of code to be within margins

--- A2/test_print_character.py
+++ A2/test_print_character.py
@@ -17,11 +17,13 @@ class TestPrintCharacter(TestCase):
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_print_character_with_inventory(self, mock_output):
         char = {'Name': 'Qumerate', 'Race': 'gnome', 'Class': 'monk', 'HP': [5, 5], 'Strength': 11, 'Dexterity': 12,
-                'Constitution': 10, 'Intelligence': 11, 'Wisdom': 10, 'Charisma': 13, 'XP': 0, 'Inventory': ['Hello','World']}
+                'Constitution': 10, 'Intelligence': 11, 'Wisdom': 10, 'Charisma': 13, 'XP': 0,
+                'Inventory': ['Hello','World']}
         print_character(char)
         actual_value = mock_output.getvalue()
         expected_value = "Name: Qumerate\nRace: gnome\nClass: monk\nHP: [5, 5]\nStrength: 11\nDexterity: 12\n" \
-                         "Constitution: 10\nIntelligence: 11\nWisdom: 10\nCharisma: 13\nXP: 0\nHere are your items:\nHello\nWorld\n"
+                         "Constitution: 10\nIntelligence: 11\nWisdom: 10\nCharisma: 13\nXP: 0\nHere are your items:\n" \
+                         "Hello\nWorld\n"
         self.assertEqual(actual_value, expected_value)
 
     @patch("sys.stdout", new_callable=io.StringIO)
@@ -36,7 +38,8 @@ class TestPrintCharacter(TestCase):
 
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_print_character_incorrect_format(self, mock_output):
-        char = {'Name': 'Qumerate', 'Race': 'gnome', 'Class': 'monk', 'HP': [5, 5], 'Strength': 11, 'Inventory': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+        char = {'Name': 'Qumerate', 'Race': 'gnome', 'Class': 'monk', 'HP': [5, 5], 'Strength': 11,
+                'Inventory': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
         print_character(char)
         actual_value = mock_output.getvalue()
         expected_value = "Warning: Error found with your list of character attributes\n"
