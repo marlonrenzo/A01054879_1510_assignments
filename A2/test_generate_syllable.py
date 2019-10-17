34c722b25458c50002f71d628d179113bdf09490
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-16 21:05:13 -0700

    Unit test copied over from lab05

---
+++ A2/test_generate_syllable.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from A2.dungeonsanddragons import generate_syllable
+
+
+class TestGenerateSyllable(TestCase):
+    def test_generate_syllable_length(self):
+        expected_value = 2
+        actual_value = len(generate_syllable())
+        self.assertEqual(actual_value, expected_value)
+
+    def test_generate_syllable_consonant(self):
+        consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
+        actual_value = generate_syllable()
+        self.assertTrue(actual_value[0] in consonant)
+
+    def test_generate_syllable_vowel(self):
+        vowel = ['a', 'e', 'i', 'o', 'u', 'y']
+        actual_value = generate_syllable()
+        self.assertTrue(actual_value[1] in vowel)
