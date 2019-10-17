40cfa80739ac8e3c35d8ee3d5af6141e4a5c31df
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-17 13:09:06 -0700

    delete a duplicate test case

--- A2/test_print_character.py
+++ A2/test_print_character.py
@@ -3,6 +3,7 @@ from A2.dungeonsanddragons import print_character
 from unittest.mock import patch
 import io
 
+
 class TestPrintCharacter(TestCase):
     @patch("sys.stdout", new_callable=io.StringIO)
     def test_print_character_no_inventory(self, mock_output):
@@ -24,16 +25,6 @@ class TestPrintCharacter(TestCase):
         expected_value = "Name: Qumerate\nRace: gnome\nClass: monk\nHP: [5, 5]\nStrength: 11\nDexterity: 12\n" \
                          "Constitution: 10\nIntelligence: 11\nWisdom: 10\nCharisma: 13\nXP: 0\nHere are your items:\n" \
                          "Hello\nWorld\n"
-        self.assertEqual(actual_value, expected_value)
-
-    @patch("sys.stdout", new_callable=io.StringIO)
-    def test_print_character_incorrect_format(self, mock_output):
-        char = {'Name': 'Qumerate', 'Race': 'gnome', 'Class': 'monk', 'HP': [5, 5], 'Strength': 11, 'Dexterity': 12,
-                'Constitution': 10, 'Intelligence': 11, 'Wisdom': 10, 'Charisma': 13, 'XP': 0, 'Inventory': []}
-        print_character(char)
-        actual_value = mock_output.getvalue()
-        expected_value = "Name: Qumerate\nRace: gnome\nClass: monk\nHP: [5, 5]\nStrength: 11\nDexterity: 12\n" \
-                         "Constitution: 10\nIntelligence: 11\nWisdom: 10\nCharisma: 13\nXP: 0\n"
         self.assertEqual(actual_value, expected_value)
 
     @patch("sys.stdout", new_callable=io.StringIO)
