3fe6d77fab16770f0d5b538a5210503c66db0796
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-17 13:07:09 -0700

    Created 3 test cases

--- A2/test_roll_hp.py
+++ A2/test_roll_hp.py
@@ -1,6 +1,23 @@
 from unittest import TestCase
+from A2.dungeonsanddragons import roll_hp
+from unittest.mock import patch
 
 
-class TestRoll_hp(TestCase):
-    def test_roll_hp(self):
-        self.fail()
+class TestRollHp(TestCase):
+    @patch("random.randint", return_value=12)
+    def test_roll_hp_barbarian_upper(self, mock_output):
+        actual_value = roll_hp("barbarian")
+        expected_value = 12
+        self.assertEqual(actual_value, expected_value)
+
+    @patch("random.randint", return_value=1)
+    def test_roll_hp_barbarian_lower(self, mock_output):
+        actual_value = roll_hp("barbarian")
+        expected_value = 1
+        self.assertEqual(actual_value, expected_value)
+
+    @patch("random.randint", return_value=17)
+    def test_roll_hp_not_a_class(self, mock_output):
+        actual_value = roll_hp("Spiderman")
+        expected_value = "Error in inputted class."
+        self.assertEqual(actual_value, expected_value)
