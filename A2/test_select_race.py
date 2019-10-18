a48608a512ad1b540cce24f65a0d63f1c9f89f34
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-18 13:49:08 -0700

    added 3 more test cases

--- A2/test_select_race.py
+++ A2/test_select_race.py
@@ -2,9 +2,30 @@ from unittest import TestCase
 from unittest.mock import patch
 from A2.dungeonsanddragons import select_race
 
-class TestSelect_class(TestCase):
+
+class TestSelectClass(TestCase):
     @patch("builtins.input", side_effect=[5])
     def test_select_class_human(self, mock_input):
         actual_value = select_race()
         expected_value = "human"
         self.assertEqual(actual_value, expected_value)
+
+    @patch("builtins.input", side_effect=[4])
+    def test_select_class_dwarf(self, mock_input):
+        actual_value = select_race()
+        expected_value = "dwarf"
+        self.assertEqual(actual_value, expected_value)
+
+    @patch("builtins.input", side_effect=[10])
+    def test_select_class_tiefling(self, mock_input):
+        actual_value = select_race()
+        expected_value = "tiefling"
+        self.assertEqual(actual_value, expected_value)
+
+    @patch("builtins.input", side_effect=[5])
+    def test_select_class_first_letter_lowercase(self, mock_input):
+        string = select_race()
+        expected_character = 'h'
+        self.assertTrue(string[0], expected_character)
+
+
