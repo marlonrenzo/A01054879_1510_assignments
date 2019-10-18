0d1226a5f23ffb52e82b946f20fdca0eb1ab7bb0
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-18 14:02:23 -0700

    created 3 test cases

--- A2/test_create_character.py
+++ A2/test_create_character.py
@@ -1,6 +1,25 @@
 from unittest import TestCase
+from A2.dungeonsanddragons import create_character
+from unittest.mock import patch
 
 
-class TestCreate_character(TestCase):
-    def test_create_character(self):
-        self.fail()
+class TestCreateCharacter(TestCase):
+    @patch("builtins.input", side_effect=[4, 4])
+    def test_create_character_length(self, mock_input):
+        actual_value = create_character(10)
+        self.assertTrue(len(actual_value) == 12)
+
+    @patch("builtins.input", side_effect=[1, 3])
+    def test_create_character_attributes(self, mock_input):
+        attributes = ['Name', 'Race', 'Class', 'HP', 'Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom',
+                      'Charisma', 'XP', 'Inventory']
+        actual_value = create_character(10)
+        for key in range(0, len(attributes)):
+            self.assertTrue(attributes[key] in actual_value)
+
+    @patch("builtins.input", side_effect=[4, 5])
+    def test_create_character_empty_inventory(self, mock_input):
+        actual_value = create_character(10)
+        inventory = actual_value['Inventory']
+        expected_value = []
+        self.assertEqual(inventory, expected_value)
