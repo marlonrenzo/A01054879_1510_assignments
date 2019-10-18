89f5df63ed93af705fbb9d096ad8e2cce2f90ac7
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-18 13:40:08 -0700

    added 3 more test cases.

--- A2/test_select_class.py
+++ A2/test_select_class.py
@@ -2,9 +2,29 @@ from unittest import TestCase
 from unittest.mock import patch
 from A2.dungeonsanddragons import select_class
 
-class TestSelect_class(TestCase):
+
+class TestSelectClass(TestCase):
     @patch("builtins.input", side_effect=[3])
-    def test_select_class(self, mock_input):
+    def test_select_class_cleric(self, mock_input):
         actual_value = select_class()
         expected_value = "cleric"
         self.assertEqual(actual_value, expected_value)
+
+    @patch("builtins.input", side_effect=[1])
+    def test_select_class_barbarian(self, mock_input):
+        actual_value = select_class()
+        expected_value = "barbarian"
+        self.assertEqual(actual_value, expected_value)
+
+    @patch("builtins.input", side_effect=[12])
+    def test_select_class_wizard(self, mock_input):
+        actual_value = select_class()
+        expected_value = "wizard"
+        self.assertEqual(actual_value, expected_value)
+
+    @patch("builtins.input", side_effect=[7])
+    def test_select_class_paladin(self, mock_input):
+        actual_value = select_class()
+        expected_value = "paladin"
+        self.assertEqual(actual_value, expected_value)
+
