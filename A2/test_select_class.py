fc6a5cd5742bf1532bd8830d99ced3b1e9b45055
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-18 10:59:31 -0700

    first test case created

--- A2/test_select_class.py
+++ A2/test_select_class.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
-
+from unittest.mock import patch
+from A2.dungeonsanddragons import select_class
 
 class TestSelect_class(TestCase):
-    def test_select_class(self):
-        self.fail()
+    @patch("builtins.input", side_effect=[3])
+    def test_select_class(self, mock_input):
+        actual_value = select_class()
+        expected_value = "cleric"
+        self.assertEqual(actual_value, expected_value)
