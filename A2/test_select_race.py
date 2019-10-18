30f5855b7f52c0464b3092884ede0a444de1b94d
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-18 10:59:06 -0700

    first test case created

--- A2/test_select_race.py
+++ A2/test_select_race.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
+from unittest.mock import patch
+from A2.dungeonsanddragons import select_race
 
-
-class TestSelect_race(TestCase):
-    def test_select_race(self):
-        self.fail()
+class TestSelect_class(TestCase):
+    @patch("builtins.input", side_effect=[5])
+    def test_select_class_human(self, mock_input):
+        actual_value = select_race()
+        expected_value = "human"
+        self.assertEqual(actual_value, expected_value)
