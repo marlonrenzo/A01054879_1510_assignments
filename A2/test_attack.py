b1a657d6a4d5e9de2769fb066d10da7469ff1960
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-20 10:41:20 -0700

    Wrote an unfinished

--- A2/test_attack.py
+++ A2/test_attack.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
+from A2.dungeonsanddragons import attack
+from A2.dungeonsanddragons import create_character
+from unittest.mock import patch
 
 
 class TestAttack(TestCase):
-    def test_attack(self):
-        self.fail()
+    @patch("random.randint", return_value=5)
+    @patch("builtins.input", side_effect=[1])
+    @patch("builtins.input", side_effect=[2])
+    def test_attack(self, mock_class, mock_race):
+        char_one = create_character(4)
+        char_two = create_character(4)
+        actual_value = attack(char_one, char_two)
+
