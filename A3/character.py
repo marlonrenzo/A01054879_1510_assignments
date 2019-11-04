3fbf149f4b895d3707e5de8ffc276446439b75b5
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-04 13:47:03 -0800

    modified create_character to match the program

--- A3/character.py
+++ A3/character.py
@@ -1,66 +1,25 @@
-from A3.SUD import roll_die
-
-
-def create_character(name_length):
-    """
-    Create a dictionary including attributes to associate to a character.
-
-    :post condition: will create a character as a dictionary of attributes
-    :return: the information of a character as a dictionary
-    """
-    print("\n\n\nLet's create a character\nStart by selecting a class.\n")
-    char_class = select_class()
-    hp = roll_hp(char_class)
-    character = {'Name': get_character_name(), 'Race': select_race(), 'Class': char_class,
-                 'HP': [hp, hp], 'Strength': roll_die(3, 6),
-                 'Dexterity': roll_die(3, 6), 'Constitution': roll_die(3, 6), 'Intelligence': roll_die(3, 6),
-                 'Wisdom': roll_die(3, 6), 'Charisma': roll_die(3, 6), 'XP': 0, 'Inventory': []}
-    return character
-
-
 def get_character_name():
     """
     Inquire the user to provide a name.
 
     :return: a string
     """
-    string = input("What is your name?")
-    return string.capitalize()
+    name = input("What is your name?").capitalize()
+    print(f"Nice to meet you {name}\n")
+    return name
 
 
-def select_race():
+def create_character():
     """
-    Ask the user to select a race from a list of varied races provided in a dictionary.
+    Create a dictionary including attributes to associate to a character.
 
-    Print out the races in a neatly-formatted manner using a for loop and f-strings.
-
-    User must enter a number that is included in the list of available selections.
-    :return: the user-selected race
+    :post condition: will create a character as a dictionary of attributes
+    :return: the information of a character as a dictionary
     """
-    race = {1: "Dwarf", 2: "Elf", 3: "Gnome", 4: "Dwarf", 5: "Human", 6: "Dragonborn", 7: "Halfling", 8: "Half-Orc", 9: "Half-Elf", 10: "Tiefling"}
-    for x in range(1, 11):
-        print(f"{x}. {race[x]}")
-
-    user_race = int(input("Select your race (by number):\n"))
-    race = race[user_race].lower()
-    return race
-
-
-def select_class():
-    """
-    Ask the user to select a class from a list of varied classes provided in a dictionary.
-
-    Print out the classes in a neatly-formatted manner using a for loop and f-strings.
-
-    User must enter a number that is included in the list of available selections.
-    :return: the user-selected class
-    """
-    classes = {1: "Barbarian", 2: "Bard", 3: "Cleric", 4: "Druid",
-               5: "Fighter", 6: "Monk", 7: "Paladin", 8: "Ranger",
-               9: "Rogue", 10: "Sorcerer", 11: "Warlock", 12: "Wizard"}
-    for x in range(1, 13):
-        print(f"{x}. {classes[x]}")
-
-    user_class = int(input("Select your class (by number):\n"))
-    selected_class = classes[user_class].lower()
-    return selected_class
+    print("\nWelcome to the Dungeon of Kather. Here are where young wizards, like yourself, learn new spells.\n"
+          "It is tradition at Castor's Thrine of Enchantment that all younglings are placed within the walls\n"
+          "of Kather to begin training. The Dungeon has 25 rooms for you to traverse through and eventually escape.\n"
+          "Use your knowledge as a spell-caster and use any items you think are useful.\n\n"
+          "Let's begin by getting to know you.........")
+    character = {'Name': get_character_name(), 'Class': 'Wizard', 'HP': [10, 10], 'Inventory': [], 'Spells': []}
+    return character
