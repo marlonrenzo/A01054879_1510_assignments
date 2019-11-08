80967a795a80ceb701f4ebaa9d8f91811be65eda
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-07 21:07:28 -0800

    added print

--- A3/monster.py
+++ A3/monster.py
@@ -1,3 +1,4 @@
 def new_monster() -> dict:
-    monster = {'Alias:': "The Monster", 'HP': [5, 5]}
+    monster = {'Alias': "The Monster", 'HP': [5, 5]}
+    print(f"\nMonster appeared! It has HP of {monster['HP'][1]}")
     return monster
