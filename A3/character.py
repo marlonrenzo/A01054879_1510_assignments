c5516dba113b50b8e72f8b4361686a86102b07d1
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-08 21:53:16 -0800

    Updated print statement

--- A3/character.py
+++ A3/character.py
@@ -18,8 +18,8 @@ def create_character() -> dict:
     """
     print("\nWelcome to the Dungeon of Kather. 'Ere are where young lads, like yourself, learn to become a warrior.\n"
           "It is tradition at Castor's Thrine that all younglings are placed within the walls of Kather to begin training.\n"
-          "The Dungeon has 25 rooms for you to traverse through and eventually escape. Use your strength as a \n"
-          "young blood-seeker and use any items you may find useful.\n\n"
+          "The Dungeon has 25 rooms for you to traverse through and eventually escape. Go forth and slash your way through \n"
+          "young blood-seeker. There are monsters in this realm. Escape the dungeon with your life and you will be unstoppable.\n\n"
           "Let's begin by getting to know you.........")
     character = {'Name': get_character_name(), 'Alias': "You", 'HP': [10, 10], 'Inventory': [], 'position': {"x": 2, "y": 2}}
     return character
