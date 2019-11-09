51d9ef489122bbf200439227b2a3302752a5ce9c
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-09 15:01:39 -0800

    Wrote all docstrings

--- A3/SUD.py
+++ A3/SUD.py
@@ -37,13 +37,13 @@ def validate_move(coordinates: dict, direction: str) -> bool:
     :post condition: will return the validity of the move as a boolean
     :return: a boolean
 
-    >>> validate_move({"x": 0, "y": 2}, 4)
+    >>> validate_move({"x": 0, "y": 2}, 'd')
     True
-    >>> validate_move({"x": 0, "y": 4}, 2)
+    >>> validate_move({"x": 0, "y": 4}, 's')
     False
-    >>> validate_move({"x": 4, "y": 2}, 4)
+    >>> validate_move({"x": 4, "y": 2}, 'd')
     False
-    >>> validate_move({"x": 3, "y": 2}, 4)
+    >>> validate_move({"x": 3, "y": 2}, 'd')
     True
     """
     if (coordinates["x"] == 4 and direction == 'd') or (coordinates["x"] == 0 and direction == 'a') or \
@@ -129,47 +129,81 @@ def check_position(position: dict, x_position: int, y_position: int) -> bool:
     :post condition: will determine whether or not the user is located at the specified coordinates with a 1 or a 0
     :return: an integer
 
+    >>> check_position({"x": 0, "y": 1}, 0, 1)
+    True
+    >>> check_position({"x": 4, "y": 4}, 4, 4)
+    True
+    >>> check_position({"x": 2, "y": 2}, 0, 0)
+    False
     """
     if position["x"] == x_position and position["y"] == y_position:
-        return True
+        return True  # return True if the position and the coordinates provided are equal
     else:
         return False
 
 
 def one_in_x_chance(x: int) -> bool:
     """
-    Generate a random number between 1 and x inclusive, compare the random number to half of x and return a boolean.
+    Generate a random number between 1 and x inclusive, compare the random number to x and return a boolean.
 
     Creates a 25% chance to return true.
     :return: a boolean
 
     """
     chance = random.randint(1, x)
-    if chance == x / 2:
+    if chance == x:
         return True
     else:
         return False
 
 
 def battle(character: dict, monster: dict) -> dict:
-    rounds = 0
-    round_cases = ["\nA battle ensues. Tension rises. Prepare for blood and despair."]
-    while character["HP"][0] > 0 and monster["HP"][0] > 0:
-        round_cases.append("\nThe battle continues.....")
-        print(round_cases[rounds])
-        remaining_health = combat_round(character, monster)
-        character['HP'][0] = remaining_health[0]
-        monster['HP'][0] = remaining_health[1]
-        rounds += 1
-    monster_drops_key = one_in_x_chance(4)
-    if monster_drops_key and character['HP'][0]:
+    """
+    Simulate a battle between character and monster.
+
+    Update character information based on the outcome of each round of combat.
+
+    Create a 1 in 4 chance that the monster will drop a key when killed.
+
+    :param character: a dict
+    :param monster: a dict
+    :precondition: character must be a properly formed dictionary
+    :precondition: monster must be a properly formed dictionary
+    :post condition: will return an updated character based on tbe outcome of the battle
+    :return: a dict
+
+    """
+    rounds = 0  # a variable to keep track how many rounds
+    round_cases = ["\nA battle ensues. Tension rises. Prepare for blood and despair."]  # what displays to begin a round
+    while character["HP"][0] > 0 and monster["HP"][0] > 0:  # loop to simulate combat rounds while both character alive
+        round_cases.append("\nThe battle continues.....")  # will show this on screen when rounds are more than 0
+        print(round_cases[rounds])  # print the right case based on what round it is
+        remaining_health = combat_round(character, monster)  # simulate a round assigning the result to a list
+        character['HP'][0] = remaining_health[0]  # update the character's health by assigning combat_round's result
+        monster['HP'][0] = remaining_health[1]  # update the character's health by assigning combat_round's result
+        rounds += 1  # show that the round will increase
+    if one_in_x_chance(4) and character['HP'][0]:  # 1 in 4 chance for the monster to drop key if character is not dead
         print('The monster dropped a key!')
-        character['Inventory'].append('key')
-    print(f"Your HP is now at {character['HP'][0]}/{character['HP'][1]}")
+        character['Inventory'].append('key')  # add a key to character's inventory
+    print(f"Your HP is now at {character['HP'][0]}/{character['HP'][1]}")  # print out the remaining character health
     return character
 
 
 def combat_round(character: dict, monster: dict) -> list:
+    """
+    Simulate a combat round between character and a monster.
+
+    Generate a random order of attack by using a nested list.
+
+    Return the updated HP of character and monster after the round occurs.
+    :param character: a dict
+    :param monster: a dict
+    :precondition: character must be a properly formed dictionary
+    :precondition: monster must be a properly formed dictionary
+    :post condition: will return a list of updated HPs of character and monster
+    :return: a list
+
+    """
     attacks_first = [[character, monster], [monster, character]]  # generates cases where either characters attack first
     random_attacker = random.randint(0, 1)  # will determine which case to use in first_attacker above
     attacker = attacks_first[random_attacker]  # initializes a variable to refer to the case used
@@ -185,27 +219,35 @@ def attack(attacker: dict, recipient: dict) -> int:
 
     :param attacker: a dictionary with character information
     :param recipient: a dictionary with character information
+    :precondition: attacker must be a properly formed dictionary
+    :precondition: recipient must be a properly formed dictionary
+    :post condition: will return HP of the recipient
     :return: an int
+
     """
-    attack_outcome = ["The attack was successful! The strike dealt significant damage!", "The attack missed!"]
+    attack_outcome = ["The attack was successful! The strike dealt significant damage!", "The attack missed!"]  # possible outcomes of an attack
     random_outcome = random.randint(0, 1)  # choose a random outcome
     print(f"{attacker['Alias']} will attack.\nNow rolling the die.....")
     attack_roll = roll_die(1, 6)  # Determine the damage an attack deals
     print(f"{attacker['Alias']} will attack {recipient['Alias']} with {attack_roll} damage!")
     print(attack_outcome[random_outcome])  # prints the random outcome of an attack (hit or miss)
     if not random_outcome:  # if attack is successful
-        remaining_hp = recipient['HP'][0] - attack_roll
-        print(f"The hit left {recipient['Alias']} with {remaining_hp}/{recipient['HP'][1]} HP")
+        remaining_hp = recipient['HP'][0] - attack_roll  # subtract damage from the recipient's HP
+        print(f"The hit left {recipient['Alias']} with {remaining_hp}/{recipient['HP'][1]} HP")  # print remaining HP
         return remaining_hp
     else:
         return recipient['HP'][0]  # original HP
 
 
-def check_alive(character) -> bool:
+def check_alive(character: dict) -> bool:
     """
     Check a character's health
-    :param character:
-    :return:
+
+    :param character: a dict
+    :precondition: character must be a properly formed dictionary
+    :post condition: will return a boolean based on the state of the character's life
+    :return: a bool
+
     """
     if character['HP'][0] <= 0:
         return False
@@ -216,6 +258,9 @@ def check_alive(character) -> bool:
 def startup():
     """
     Print the startup scenario.
+
+    :return: nothing
+
     """
     print("You are awoken in a dark concrete room. Doors surround you on all sides.\n"
           "A trap door in the middle of the room protrudes the ceiling. It requires a key. \n"
@@ -228,13 +273,24 @@ def startup():
 def get_user_choice_battle() -> bool:
     """
     Grab user input to determine if they want to run from or fight monster.
+
     :return: a boolean
+
     """
     user_input = bool(input("Would you like to fight[1] or run[0]?"))
     return user_input
 
 
 def heal(health: int) -> int:
+    """
+    Heal the character by increasing their HP..
+
+    :param health: an int
+    :precondition: health must be an integer greater than 0
+    :post condition: will return a character's updated HP
+    :return: an int
+
+    """
     if health < 9:
         health += 2
         print(f"Whilst moving around, you have healed a bit. You're now at {health}/10 HP ")
@@ -247,13 +303,21 @@ def heal(health: int) -> int:
 def check_at_exit_with_key(position: dict, inventory: list) -> bool:
     """
     Determine if a character is at the exit coordinates with a key.
+
     :param position: a dict
     :param inventory: a list
     :precondition: position must be a dictionary containing valid coordinates
     :precondition: inventory must be a list
     :return: a boolean
+
+    >>> check_at_exit_with_key({"x": 0, "y": 1}, [])
+    False
+    >>> check_at_exit_with_key({"x": 4, "y": 4}, ['key'])
+    False
+    >>> check_at_exit_with_key({"x": 2, "y": 2}, ['key'])
+    True
     """
-    if position['x'] == 2 and position['y'] == 2 and inventory == ['key']:
+    if check_position(position, 2, 2) and inventory == ['key']:
         return True
     else:
         return False
@@ -261,8 +325,10 @@ def check_at_exit_with_key(position: dict, inventory: list) -> bool:
 
 def user_win():
     """
-    Print a line to tell the user they won the game!
+    Print a line to tell the user they won the game.
+
     :return: nothing
+
     """
     print("Greatest salutations young lad! You've escaped Kather's tunnels! You will make a noble warrior one day!")
     return
@@ -273,11 +339,14 @@ def fight_monster(character: dict) -> dict:
     Ask user if they would like to fight or run from battle.
 
     Create a one in ten chance of getting damaged if character decides to run.
-    :param character:
-    :return:
+    :param character: a dict
+    :precondition: character must be a properly formatted dictionary
+    :post condition: will return character
+    :return: a dict
+
     """
     if input("Would you like to fight [1] or run [0]?"):
-        if one_in_x_chance(10):
+        if one_in_x_chance(10):  # will modify a character's HP with a one in ten chance
             damage = roll_die(1, 4)
             character['HP'][0] -= damage
             print(f"As you were running away, the monster swiped you and you lost {damage} HP!")
@@ -289,10 +358,12 @@ def fight_monster(character: dict) -> dict:
 def monster_encounter(character: dict) -> dict:
     """
     Determine the chance of a monster encounter, update the character after a battle if monster present.
+
     :param character: a dict
     :precondition: character must be a dictionary
     :post condition: will return an (non)updated dictionary
     :return: a dict
+
     """
     if one_in_x_chance(4):  # create a 1 in 4 chance to encounter a monster
         monster = new_monster()
@@ -303,6 +374,12 @@ def monster_encounter(character: dict) -> dict:
 
 
 def run_game():
+    """
+    Run the SUD.
+
+    The main flow of the game that calls all functions.
+    :return: nothing
+    """
     character = create_character()  # creates a new character
     startup()  # print the starting scenario
     while True:
