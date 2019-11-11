e21a312a70adabb658cb3674c8a04072b8e5ce68
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-11 14:23:11 -0800

    4 test cases created.

--- A3/test_validate_move.py
+++ A3/test_validate_move.py
@@ -1,6 +1,20 @@
 from unittest import TestCase
+from A3.SUD import validate_move
 
 
 class TestValidateMove(TestCase):
-    def test_validate_move(self):
-        self.fail()
+    def test_validate_move_right_valid(self):
+        actual_value = validate_move({"x": 0, "y": 2}, 'd')
+        self.assertTrue(actual_value)
+
+    def test_validate_move_down(self):
+        actual_value = validate_move({"x": 0, "y": 4}, 's')
+        self.assertFalse(actual_value)
+
+    def test_validate_move_right_invalid(self):
+        actual_value = validate_move({"x": 4, "y": 2}, 'd')
+        self.assertFalse(actual_value)
+
+    def test_validate_move_right_valid_2(self):
+        actual_value = validate_move({"x": 3, "y": 2}, 'd')
+        self.assertTrue(actual_value)
