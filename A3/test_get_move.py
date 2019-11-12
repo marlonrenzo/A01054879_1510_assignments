ad4466fb657813ec9f637d7cfd1ab19a09607bdd
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-11 16:37:39 -0800

    Two test cases created

--- A3/test_get_move.py
+++ A3/test_get_move.py
@@ -1,7 +1,17 @@
 from unittest import TestCase
 from A3.SUD import get_move
+from unittest.mock import patch
 
 
 class TestGetMove(TestCase):
-    def test_get_move(self):
-        self.fail()
+    @patch("builtins.input", side_effect=['UPPER'])
+    def test_get_move_uppercase(self, mock_input):
+        actual_value = get_move()
+        expected_value = 'upper'
+        self.assertEqual(actual_value, expected_value)
+
+    @patch("builtins.input", side_effect=['lower'])
+    def test_get_move_lowercase(self, mock_input):
+        actual_value = get_move()
+        expected_value = 'lower'
+        self.assertEqual(actual_value, expected_value)
