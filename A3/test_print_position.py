ba946d6084f3c896de1898851cbb974a74a61af1
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-11 17:38:08 -0800

    4 test cases copied over from lab

--- A3/test_print_position.py
+++ A3/test_print_position.py
@@ -1,6 +1,50 @@
 from unittest import TestCase
+from A3.SUD import print_position
+from unittest.mock import patch
+import io
 
 
-class TestPrint_position(TestCase):
-    def test_print_position(self):
-        self.fail()
+class TestShowPosition(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_position_top_left(self, mock_output):
+        print_position({"x": 0, "y": 0})
+        actual_value = mock_output.getvalue()
+        expected_value = '\n\n[x] [ ] [ ] [ ] [ ] \n\n' \
+                             '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                             '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                             '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                             '[ ] [ ] [ ] [ ] [ ] \n\n'
+        self.assertEqual(actual_value, expected_value)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_position_top_right(self, mock_output):
+        print_position({"x": 4, "y": 0})
+        actual_value = mock_output.getvalue()
+        expected_value = '\n\n[ ] [ ] [ ] [ ] [x] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n'
+        self.assertEqual(actual_value, expected_value)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_position_bottom_right(self, mock_output):
+        print_position({"x": 4, "y": 4})
+        actual_value = mock_output.getvalue()
+        expected_value = '\n\n[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [x] \n\n'
+        self.assertEqual(actual_value, expected_value)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_position_bottom_left(self, mock_output):
+        print_position({"x": 0, "y": 4})
+        actual_value = mock_output.getvalue()
+        expected_value = '\n\n[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[x] [ ] [ ] [ ] [ ] \n\n'
+        self.assertEqual(actual_value, expected_value)
