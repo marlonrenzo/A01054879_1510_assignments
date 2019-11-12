f8744ef17f72c9ae142e4cf1988af08d5dc066ac
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-11 16:04:55 -0800

    4 test cases created to test all four directions

--- A3/test_move_character.py
+++ A3/test_move_character.py
@@ -3,5 +3,22 @@ from A3.SUD import move_character
 
 
 class TestMoveCharacter(TestCase):
-    def test_move_character(self):
-        self.fail()
+    def test_move_character_right(self):
+        actual_value = move_character({"x": 0, "y": 0}, 'd')
+        expected_value = {'x': 1, 'y': 0}
+        self.assertEqual(actual_value, expected_value)
+
+    def test_move_character_up(self):
+        actual_value = move_character({"x": 3, "y": 3}, 'w')
+        expected_value = {'x': 3, 'y': 2}
+        self.assertEqual(actual_value, expected_value)
+
+    def test_move_character_left(self):
+        actual_value = move_character({"x": 1, "y": 1}, 'a')
+        expected_value = {'x': 0, 'y': 1}
+        self.assertEqual(actual_value, expected_value)
+
+    def test_move_character_down(self):
+        actual_value = move_character({"x": 0, "y": 1}, 's')
+        expected_value = {'x': 0, 'y': 2}
+        self.assertEqual(actual_value, expected_value)
