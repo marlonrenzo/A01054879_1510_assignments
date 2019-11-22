f8b63c27f08ae8e543ffaf96a5ccb9f5bef42fbf
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-21 20:27:10 -0800

    got rid of else statement

--- A4/Question 1.py
+++ A4/Question 1.py
@@ -6,8 +6,6 @@ def remove_multiples(set_of_nums, increment):
     for i in set_of_nums:  # loop thru the list of numbers
         if i % increment != 0 or i == increment:
             new_numbers.append(i)  # append the number if it's not the increment itself or it's not a multiple
-        elif i % increment == 0:  # if the number is a multiple, don't append it to the updated list
-            continue
     return new_numbers
 
 
@@ -17,13 +15,12 @@ def eratosthenes(upperbound):
     while numbers[counter] < upperbound ** (1/2):  # loop thru numbers until it reaches the square root of upperbound
         numbers = remove_multiples(numbers, numbers[counter])  # update  numbers by removing multiples of current number
         counter += 1  # move on to the next number to check
-
     return numbers
 
 
 def main():
     doctest.testmod()
-    print(eratosthenes(100))
+    print(eratosthenes(30))
 
 
 if __name__ == '__main__':
