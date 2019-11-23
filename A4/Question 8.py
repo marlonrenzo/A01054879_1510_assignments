f64c7726d007335e1c2f5f054cb4bfbba11bae13
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-22 22:18:07 -0800

    added inline comments

--- A4/Question 8.py
+++ A4/Question 8.py
@@ -17,9 +17,9 @@ def find_largest(values: dict, min_digit: int, max_digit: int) -> int:
     >>> find_largest(test, 0, 9)
     8
     """
-    most_bars = 0
-    for number in range(min_digit, max_digit):
-        if values[number] > most_bars:
+    most_bars = 0  # set an arbitrary number
+    for number in range(min_digit, max_digit):  # loop through the numbers between min_digit and max_digit
+        if values[number] > most_bars:  # if loop is greater than the current larger # of bars, number is now largest
             most_bars = number
     return most_bars
 
@@ -33,11 +33,11 @@ def im_not_sleepy() -> str:
     :return: a str
 
     """
-    digit_values = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
-    second = find_largest(digit_values, 0, 3)
-    third = find_largest(digit_values, 0, 6)
+    digit_values = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}  # set values of digits with # of bars
+    second = find_largest(digit_values, 0, 3)  # first digit is skipped as it is automatically a 1
+    third = find_largest(digit_values, 0, 6)  # each digit is found with a specific range
     fourth = find_largest(digit_values, 0, 10)
-    bars = 2 + digit_values[second] + digit_values[third] + digit_values[fourth]
+    bars = 2 + digit_values[second] + digit_values[third] + digit_values[fourth]  # sum of bars is all digits together
     return f"The time requiring the most amount of bars is:\n1{second}:{third}{fourth} with {bars} bars"
 
 
