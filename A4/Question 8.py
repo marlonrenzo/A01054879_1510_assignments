9e2fcbfeb4672d9f698f1f5575da6cce22c37b9b
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-22 22:02:18 -0800

    some docstrings written

--- A4/Question 8.py
+++ A4/Question 8.py
@@ -1,4 +1,20 @@
-def find_largest(values, min_digit, max_digit):
+import doctest
+
+
+def find_largest(values: dict, min_digit: int, max_digit: int) -> int:
+    """
+    Determine the number with the largest value within a dictionary between minimun and maximun digits.
+
+    :param values: a dict
+    :param min_digit: an int
+    :param max_digit: an int
+    :precondition: values must be a properly formatted dictionary with the correct values
+    :precondition: min and max digits
+    :post condition: will return the key in the dictionary with the highest value
+    :return: an int
+
+    >>>find_largest()
+    """
     most_bars = 0
     for number in range(min_digit, max_digit):
         if values[number] > most_bars:
@@ -6,7 +22,15 @@ def find_largest(values, min_digit, max_digit):
     return most_bars
 
 
-def im_not_sleepy():
+def im_not_sleepy() -> str:
+    """
+    Determine the time that requires the most amount of bars to display a given time.
+
+    Find the greatest amount for each digit, find the sum and print the time.
+
+    :return: a str
+
+    """
     digit_values = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
     second = find_largest(digit_values, 0, 3)
     third = find_largest(digit_values, 0, 6)
@@ -16,6 +40,7 @@ def im_not_sleepy():
 
 
 def main():
+    doctest.testmod()
     print(im_not_sleepy())
 
 
