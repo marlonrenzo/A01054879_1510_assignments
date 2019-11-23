fec3ccee2515eb0601797adf1a39c41f570c97a2
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-22 20:51:56 -0800

    Wrote doctests and docstrings

--- A4/Question 1.py
+++ A4/Question 1.py
@@ -1,7 +1,22 @@
 import doctest
 
 
-def remove_multiples(set_of_nums, increment):
+def remove_multiples(set_of_nums: list, increment: int) -> list:
+    """
+    Remove all multiples of an increment.
+
+    :param set_of_nums: a list
+    :param increment: an int
+    :precondition: set_of_nums must be a non-empty
+    :precondition: increment must be a positive int
+    :post condition: will return a new list without the multiples of the increment provided
+    :return: a list
+
+    >>> remove_multiples([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)
+    [1, 2, 3, 5, 7, 9]
+    >>> remove_multiples([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 3)
+    [1, 2, 3, 4, 5, 7, 8, 10, 11]
+    """
     new_numbers = []  # create a list that will be returned with an updated set of numbers
     for i in set_of_nums:  # loop thru the list of numbers
         if i % increment != 0 or i == increment:
@@ -9,7 +24,20 @@ def remove_multiples(set_of_nums, increment):
     return new_numbers
 
 
-def eratosthenes(upperbound):
+def eratosthenes(upperbound: int) -> list:
+    """
+    Remove all non-prime numbers in a range between 2 and a upperbound.
+
+    :param upperbound: an int
+    :precondition: upperbound must be a positive int
+    :post condition: will return an updated list of prime numbers between 2 and upperbound
+    :return: return
+
+    >>> eratosthenes(30)
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    >>> eratosthenes(100)
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    """
     numbers = list(range(2, upperbound + 1))  # create a list between 0 and the upperbound
     counter = 0  # begin the counter at 2 as 1 and zero are not prime numbers
     while numbers[counter] < upperbound ** (1/2):  # loop thru numbers until it reaches the square root of upperbound
@@ -19,8 +47,15 @@ def eratosthenes(upperbound):
 
 
 def main():
+    """
+    Call all functions to run the program.
+
+    :return: None
+
+    """
     doctest.testmod()
-    print(eratosthenes(30))
+    print(eratosthenes(100))
+
 
 
 if __name__ == '__main__':
