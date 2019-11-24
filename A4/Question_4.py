d3b42a60531bb2aa4120daffcfb56e01a4bfd329
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-23 21:49:53 -0800

    Decomposed selection_sort

--- A4/Question_4.py
+++ A4/Question_4.py
@@ -1,6 +1,29 @@
 import doctest
 
 
+def find_lowest_value(items: list) -> int:
+    """
+    Determine the lowest value in a list of items.
+
+    :param items: a list
+    :return: an int
+
+    >>> test = [1, 10, 10, 10]
+    >>> find_lowest_value(test)
+    1
+
+    >>> test = [1000, 1001]
+    >>> find_lowest_value(test)
+    1000
+
+    """
+    lowest_num = items[0]  # an arbitrary number to create an initial lowest number
+    for number in items:  # loop through the numbers
+        if number < lowest_num:  # if the number is lower than current value of lowest_num, number is the new low num
+            lowest_num = number
+    return lowest_num
+
+
 def selection_sort(items: list) -> list:
     """
     Sort items in a list without the use of builtin functions.
@@ -15,18 +38,18 @@ def selection_sort(items: list) -> list:
     >>> selection_sort([15, 14, 13, 12, 11])
     [11, 12, 13, 14, 15]
     """
-    if not len(items):
-        raise ValueError("Cannot enter an empty or non-sortable list")
-    sorted_items = []
-    while items:
-        lowest_num = items[0]
-        for number in items:
-            if number < lowest_num:
-                lowest_num = number
-        items.remove(lowest_num)
-        sorted_items.append(lowest_num)
-
-    return sorted_items
+    try:
+        sorted(items)  # try an action that will raise a TypeError if items is a non-sortable list
+        items[0] = items[0]  # try an action that will raise an IndexError if items is an empty list
+    except TypeError or IndexError:  # except the two errors if they are raised by the above statements
+        raise ValueError("The input should be a non-empty sortable list.")
+    else:  # run the code as normal
+        sorted_items = []  # initiate a new list that will have the sorted items
+        while items:  # loop while items has elements in it
+            lowest_value = find_lowest_value(items)  # find the lowest number currently
+            items.remove(lowest_value)  # remove the lowest number from the original list
+            sorted_items.append(lowest_value)  # append the lowest number to the new list
+        return sorted_items
 
 
 def main():
@@ -37,7 +60,7 @@ def main():
 
     """
     doctest.testmod()
-    print(selection_sort([]))
+    print(selection_sort(['yo', 'yooo', 'y', 'yoo']))
 
 
 if __name__ == '__main__':
