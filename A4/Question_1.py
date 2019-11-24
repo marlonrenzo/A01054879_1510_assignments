8e15539373236fd90eed35f0cebf93abf230912e
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-23 22:22:02 -0800

    Changed around numbers in the range

--- A4/Question_1.py
+++ A4/Question_1.py
@@ -16,6 +16,7 @@ def remove_multiples(set_of_nums: list, increment: int) -> list:
     [1, 2, 3, 5, 7, 9]
     >>> remove_multiples([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 3)
     [1, 2, 3, 4, 5, 7, 8, 10, 11]
+
     """
     new_numbers = []  # create a list that will be returned with an updated set of numbers
     for i in set_of_nums:  # loop thru the list of numbers
@@ -29,7 +30,7 @@ def eratosthenes(upperbound: int) -> list:
     Remove all non-prime numbers in a range between 2 and a upperbound.
 
     :param upperbound: an int
-    :precondition: upperbound must be a positive int
+    :precondition: upperbound must be a positive int equal or greater than 2
     :post condition: will return an updated list of prime numbers between 2 and upperbound
     :return: return
 
@@ -37,10 +38,11 @@ def eratosthenes(upperbound: int) -> list:
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     >>> eratosthenes(100)
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
     """
     if upperbound < 0 or type(upperbound) != int:
         raise ValueError("The value is not valid. The upperbound should be a positive integer.")
-    numbers = list(range(2, upperbound + 1))  # create a list between 0 and the upperbound
+    numbers = list(range(2, upperbound + 1))  # create a list between 0 and the upperbound inclusive
     counter = 0  # begin the counter at 2 as 1 and zero are not prime numbers
     while numbers[counter] < upperbound ** (1/2):  # loop thru numbers until it reaches the square root of upperbound
         numbers = remove_multiples(numbers, numbers[counter])  # update  numbers by removing multiples of current number
@@ -56,7 +58,7 @@ def main():
 
     """
     doctest.testmod()
-    print(eratosthenes(100))
+    print(eratosthenes(2))
 
 
 if __name__ == '__main__':
