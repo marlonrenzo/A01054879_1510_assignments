d3db9341ce5b0c8bbafe4ced30602e36681a37d9
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-23 17:46:40 -0800

    Wrote all cases

--- A4/Question 6.py
+++ A4/Question 6.py
@@ -1,13 +1,23 @@
 """
-from unittest import TestCase
-from A4.question_6 import line_intersect
+Six most appropriate unittests for question_6:
 
+1. Parallel Lines, AKA there are no intersections, so test two lines with the exact same slope but at different
+points. The expected output is None, because the two lines never intersect.
 
-class TestLineIntersect(TestCase):
-    def test_line_intersect(self):
-        line_one = []
-        line_two = []
-        actual = line_intersect(line_one, line_two)
-        expected = []
-        self.assertTrue(actual, expected)
+2. A set of parallel lines that are really close together, but never meet. This should sill return None. In this case we
+would test lines that are 0.1 units away to ensure the program considers the slightest difference, and that no accuracy
+is lost throughout any calculations within the program.
+
+3. A set of lines like in case 2, but this time, with two lines with negative slopes (going down and to the right).
+We need to ensure the program still recognizes these lines as parallel and should return None.
+
+4. A regular set of lines, one with a negative slope (i.e. moving down and to the right) and a positive slope (i.e.
+moving up and to the left). This should return ONE POINT as there is only one clear intersection.
+
+5. A set of lines with slopes so close, that the difference between them is 0.00001. The lines will eventually meet,
+so we need to ensure the program recognizes such a small difference and returns one point of intersection.
+
+6. We will test the same case as 5 (with two lines almost, but not quite parallel), but this time, have the two lines as
+negative slopes (going down and to the right). The program should recognize that these lines still aren't parallel and
+it should return ONE point of intersection.
 """
