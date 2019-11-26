be0a0d584e634b99df01fba461fc1d3182c4bfcf
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-26 12:58:00 -0800

    Changed around some test cases

--- A4/Question_6.py
+++ A4/Question_6.py
@@ -8,16 +8,16 @@ points. The expected output is None, because the two lines never intersect.
 would test lines that are 0.1 units away to ensure the program considers the slightest difference, and that no accuracy
 is lost throughout any calculations within the program.
 
-3. A set of lines like in case 2, but this time, with two lines with negative slopes (going down and to the right).
-We need to ensure the program still recognizes these lines as parallel and should return None.
-
-4. A regular set of lines, one with a negative slope (i.e. moving down and to the right) and a positive slope (i.e.
+3. A regular set of lines, one with a negative slope (i.e. moving down and to the right) and a positive slope (i.e.
 moving up and to the left). This should return ONE POINT as there is only one clear intersection.
 
-5. A set of lines with slopes so close, that the difference between them is 0.00001. The lines will eventually meet,
+4. A set of lines with slopes so close, that the difference between them is 0.00001. The lines will eventually meet,
 so we need to ensure the program recognizes such a small difference and returns one point of intersection.
 
-6. Test two lines with the same exact slopes, with the same points. These lines are laying on top of one another and
+5. Test two lines with the same exact slopes, with the same points. These lines are laying on top of one another and
 this should return the first line argument.
+
+6. Have the first argument be a line, and the second argument be a dot ( i.e. [[1, 1], [1, 1]] ) somewhere along the
+first line. Since the two arguments meet, this should return the second argument. 
 
 """
